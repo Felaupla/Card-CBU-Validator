90a350a58e3dc98b3c2944d96e4ec52448748d41
unknown <felipe.planes@gmail.com>
2023-04-26 14:38:08 -0300

    refactor app.js on BE

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
+const {validate} = require ("../creditCardValidator")
 const app = express();
-const { validate } = require("../creditCardValidator.js");
-const multer = require("multer");
-const joi = require("joi");
-const fs = require("fs");
-const upload = multer({ dest: "uploads/" }); // specify the destination folder for uploaded files
 
 app.use(express.json({ limit: "3mb" }));
 
@@ -17,7 +13,8 @@ app.post("/validate", async (req, res) => {
     invalid: new Set(),
   };
   for (let cardNumber of cardNumbers) {
-    let isValid = validate(cardNumber);
+    let result= validate(cardNumber)
+    let isValid = result.response
     if (isValid) {
       // si fuese un array, sería push
       response.valid.add(cardNumber);
@@ -29,22 +26,9 @@ app.post("/validate", async (req, res) => {
     // esto es para entregar el resultado (quitar los elementos del set en un array)
     valid: [...response.valid],
     invalid: [...response.invalid],
-  });
-});
 
-app.post("/validateFile", upload.single("file"), (req, res) => {
-  // get the uploaded file from the request object
-  // read the file contents and parse the JSON data
-  const fileContents = fs.readFileSync(req.file.path, "utf8");
-  const cardNumbers = fileContents.split("\n");
-  const results = [];
-  // access the specific field you are interested in
-  // Validate each credit card number
-  cardNumbers.forEach((cardNumber) => {
-    const validationResult = validate(cardNumber.trim(","));
-    results.push(validationResult);
   });
-  res.json(results);
-});
+  });
+
 
 module.exports = app;
