9822bb9a3a22a91658f23b7267286e31d89a9e3f
Felipe Augusto Planes <105817963+Felaupla@users.noreply.github.com>
2023-05-15 12:11:51 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactRefresh from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
