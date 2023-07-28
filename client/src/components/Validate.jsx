89e9a519e2718f933d1906d0117eee72a9694316
Felipe Augusto Planes <105817963+Felaupla@users.noreply.github.com>
2023-07-28 12:06:06 -0300

    Update Validate.jsx

--- client/src/components/Validate.jsx
+++ client/src/components/Validate.jsx
@@ -1,16 +1,58 @@
 import { useState } from "react";
 import "./Validate.css";
-import { Input, Button, Box, Text } from "@chakra-ui/react";
-import axios from "axios";
+import { Input, Button, Box, Text } from '@chakra-ui/react'
 
 export default function Validate() {
   const [cardNumber, setCardNumber] = useState("");
   const [response, setResponse] = useState(null);
+  //const deploy_host = import.meta.env.VITE_DEPLOY_HOST;
   const deploy_host = "https://validatecreditcard-production.up.railway.app";
 
   const handleValidate = async () => {
     try {
-      // ... rest of the code for handleValidate function ...
+      if (cardNumber.trim() === "") {
+        setResponse({
+          valid: false,
+          type: "Unknown",
+          message: "Card number must not be empty.",
+        });
+        return;
+      }
+      if (cardNumber.length < 15) {
+        setResponse({
+          valid: false,
+          type: "Unknown",
+          message: "Incomplete card number.",
+        });
+        return;
+      }
+
+      // Detect card type
+      let cardType;
+      if (/^4[0-9]{12}(?:[0-9]{3})?$/.test(cardNumber)) {
+        cardType = "Visa";
+      } else if (/^5[1-5][0-9]{14}$/.test(cardNumber)) {
+        cardType = "Mastercard";
+      } else if (/^3[47][0-9]{13}$/.test(cardNumber)) {
+        cardType = "American Express";
+      } else if (/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/.test(cardNumber)) {
+        cardType = "Diners Club";
+      } else {
+        cardType = "Unknown";
+      }
+
+      const response = await fetch(`${deploy_host}/validateunique`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ card: [cardNumber] }), // Modify request body
+      });
+      const data = await response.json(); // Parse response as JSON
+      setResponse({
+        valid: data,
+        type: cardType,
+      }); // Set response state to object with boolean value and card type
     } catch (error) {
       console.log(error);
     }
@@ -18,16 +60,18 @@ export default function Validate() {
 
   return (
     <Box>
-      <Box border="4px" borderColor="gray.300" borderRadius={20}>
+      <Box border='4px' borderColor='gray.300' borderRadius={20}>
         <Input
           className="input-group"
           placeholder="Test a CardNumber"
           value={cardNumber.replace(/.(?=.{6})/g, "*")}
           onChange={(e) => setCardNumber(e.target.value)}
           m={[2, 3]}
-          w="60%"
+          w="65%"
         />
-        <Button onClick={handleValidate}>Validate</Button>
+        <Button onClick={handleValidate}>
+          Validate
+        </Button>
       </Box>
       {response !== null && (
         <Text className={response.valid ? "success" : "error"}>
